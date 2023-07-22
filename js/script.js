b6426c9436462ee20e93a06f7403721f43216579
Zeffal <william.nathanael3520@gmail.com>
2023-07-22 17:41:24 +0700

    Improvement

--- js/script.js
+++ js/script.js
@@ -1,7 +1,18 @@
 //Toggle class active
+// Search element with selector/id name Navbar-nav
 const navbarNav = document.querySelector(".Navbar-nav");
 
 // When click sidebar toggle, activing css .Navbar-nav.active
 document.querySelector("#hamburger-menu").onclick = () => {
   navbarNav.classList.toggle("active");
 };
+
+// Close sidebar without click the sidebar
+// Search element with selector/id name hamburger-menu
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
